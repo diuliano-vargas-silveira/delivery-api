cc7dc052bd79ab730b487607af9c01804df24c99
Diuliano Vargas da Silveira <diulidsvargas@gmail.com>
2024-05-17 13:32:12 -0300

    feat: adding open api and give permission to access it

--- src/main/java/br/com/delivery/deliveryapi/config/SecurityConfig.java
+++ src/main/java/br/com/delivery/deliveryapi/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -26,7 +27,7 @@ public class SecurityConfig {
         security
                 .csrf((AbstractHttpConfigurer::disable))
                 .authorizeHttpRequests(authorize -> {
-                    authorize.requestMatchers("/api/authorization/**").permitAll();
+                    authorize.requestMatchers("/api/authorization/**", "/", "/api-docs","/api-docs/**","/swagger-resources/**","/swagger-ui/**").permitAll();
                     authorize.requestMatchers("/restaurant/product").hasAuthority(Role.RESTAURANT.name());
                     authorize.anyRequest().authenticated();
                 }).sessionManagement(session -> {
